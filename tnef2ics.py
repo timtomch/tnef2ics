812a8e25c5034724ac8c96fb0d4dea91a28380cb
Thomas Guignard <tom@timtom.ch>
2022-06-07 17:54:27 -0400

    Replaced event description with cleaned up HTML

--- tnef2ics.py
+++ tnef2ics.py
@@ -6,20 +6,47 @@ from datetime import datetime, timezone, timedelta as td
 import re
 import pytz
 import sys
+from abc import ABC
+from html.parser import HTMLParser
 
-if len(sys.argv) < 2:
-    sys.exit("First argument must be the path to a winmail.dat file to convert!")
-else:
-    infile = str(sys.argv[1])
+# Simple HTML cleanup helper class for the event description
+class HTMLFilter(HTMLParser, ABC):
+    """
+    A simple no dependency HTML -> TEXT converter.
+    Based on https://stackoverflow.com/a/62180428
+    Usage:
+          str_output = HTMLFilter.convert_html_to_text(html_input)
+    """
+    def __init__(self, *args, **kwargs):
+        self.text = ''
+        self.in_body = False
+        super().__init__(*args, **kwargs)
 
-if len(sys.argv) == 3:
-    outfile = str(sys.argv[2])
-else:
-    outfile = 'invite.ics'
+    def handle_starttag(self, tag: str, attrs):
+        if tag.lower() == "body":
+            self.in_body = True
+        elif tag.lower() in ("br", "p"):
+            self.text += "\n" 
 
-newcal = Calendar()
-newevent = Event()
+    def handle_endtag(self, tag):
+        if tag.lower() == "body":
+            self.in_body = False
 
+    def handle_startendtag(self, tag):
+        if tag.lower() == "br":
+            self.text += "\n"
+
+    def handle_data(self, data):
+        if self.in_body:
+            self.text += data
+
+    @classmethod
+    def convert_html_to_text(cls, html: str) -> str:
+        f = cls()
+        f.feed(html)
+        return f.text.strip()  
+
+# Helper function to extract timezone information
 def parse_tz(tzstr):
     p = re.compile('UTC([+-])(\d\d):(\d\d)')
     m = p.search(tzstr)
@@ -37,6 +64,23 @@ def parse_tz(tzstr):
         return timezone(-tz_offset if sign == '-' else tz_offset)
 
 
+# Process script arguments
+
+if len(sys.argv) < 2:
+    sys.exit("First argument must be the path to a winmail.dat file to convert!")
+else:
+    infile = str(sys.argv[1])
+
+if len(sys.argv) == 3:
+    outfile = str(sys.argv[2])
+else:
+    outfile = 'invite.ics'
+
+# Init icalendar objects
+newcal = Calendar()
+newevent = Event()
+
+# Process TNEF input file by looking for some properties
 with open(infile, "rb") as tneffile:
     tnefobj = TNEF(tneffile.read())
     
@@ -44,7 +88,7 @@ with open(infile, "rb") as tneffile:
     # Not sure how we're supposed to look up values in this list.
     # Going through all of them and looking for the ones I'm looking for instead
     
-    newcal.add('prodid', '-//TNEF to ICS converter//labs.timtom.ch//')
+    newcal.add('prodid', '-//TNEF to ICS converter script//https://github.com/timtomch/tnef2ics//')
     newcal.add('version', '2.0')
     
     for value in tnefobj.mapiprops:
@@ -62,10 +106,7 @@ with open(infile, "rb") as tneffile:
             orgname = value.data
         if value.name_str == 'MAPI_SENDER_EMAIL_ADDRESS"':
             orgemail = value.data
-    
-    #starttime_object = datetime.strptime(starttime, '%Y-%m-%d %H:%M:%s')
-    
-    
+
     if timezone != '':
         tz = parse_tz(tzstring)
         starttime = starttime.replace(tzinfo=tz)
@@ -74,11 +115,11 @@ with open(infile, "rb") as tneffile:
     newevent.add('dtstart',starttime)
     newevent.add('dtend',endtime)
     
-    newevent.add('description',tnefobj.body)
-    
+    newevent.add('description',HTMLFilter.convert_html_to_text(tnefobj.htmlbody))
+
+# Add the event we just created to the calendar object
 newcal.add_component(newevent)
 
-
-
+# Write out the resulting calendar object to the specified output file path
 with open(outfile, "wb") as outfile:
     outfile.write(newcal.to_ical())
